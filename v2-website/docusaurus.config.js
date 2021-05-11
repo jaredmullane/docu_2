4f076ffb74003b98cd3a6ba79842da46bf9dda6d
shanku9 <ankursh@fb.com>
2021-05-11 11:59:42 -0700

    updated docs

---
+++ v2-website/docusaurus.config.js
@@ -0,0 +1,121 @@
+module.exports={
+  "title": "Magma Documentation",
+  "tagline": "Bring more people online by enabling operators with open, flexible, and extensible network solutions",
+  "url": "https://magmacore.org",
+  "baseUrl": "/",
+  "organizationName": "magma",
+  "projectName": "magma",
+  "scripts": [
+    "https://buttons.github.io/buttons.js"
+  ],
+  "favicon": "img/icon.png",
+  "customFields": {
+    "disableTitleTagline": true
+  },
+  "onBrokenLinks": "log",
+  "onBrokenMarkdownLinks": "log",
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "showLastUpdateAuthor": true,
+          "showLastUpdateTime": true,
+          "editUrl": "https://github.com/magma/magma/tree/master/v2-website/docs",
+          "path": "docs/", // FS content path (./docs)
+          "routeBasePath": "docs", // Url subpath (http://localhost:3000/docs)
+          "sidebarPath": require.resolve("./sidebars.json"),
+          // "onlyIncludeVersions": ['current','1.4.0','1.3.0'],
+        },
+        "blog": {},
+        "theme": {
+          "customCss": "../src/css/customTheme.css"
+        }
+      }
+    ]
+  ],
+  "plugins": [],
+  "themeConfig": {
+    "navbar": {
+      "title": "Magma Documentation",
+      "logo": {
+        "src": "img/magma-logo.svg"
+      },
+      "items": [
+        {
+          "href": "https://magmacore.org",
+          "label": "Home",
+          "position": "left"
+        },
+        {
+          "to": "docs/",
+          "label": "Docs",
+          "position": "left"
+        },
+        {
+          "href": "https://github.com/magma",
+          "label": "Code",
+          "position": "left"
+        },
+        {
+          "href": "https://magmacore.org/community",
+          "label": "Community",
+          "position": "left"
+        },
+        {
+          "label": "Version",
+          "to": "docs",
+          "position": "right",
+          "items": [
+            {
+              "label": "1.5.0",
+              "to": "docs/",
+              "activeBaseRegex": "docs/(?!1.0.0|1.0.1|1.1.0|1.2.0|1.3.0|1.4.0|1.5.0|next)"
+            },
+            {
+              "label": "1.4.0",
+              "to": "docs/1.4.0/"
+            },
+            {
+              "label": "1.3.0",
+              "to": "docs/1.3.0/"
+            },
+            {
+              "label": "1.2.0",
+              "to": "docs/1.2.0/"
+            },
+            {
+              "label": "1.1.0",
+              "to": "docs/1.1.0/"
+            },
+            {
+              "label": "1.0.1",
+              "to": "docs/1.0.1/"
+            },
+            {
+              "label": "1.0.0",
+              "to": "docs/1.0.0/"
+            },
+            {
+              "label": "Master/Unreleased",
+              "to": "docs/next/",
+              "activeBaseRegex": "docs/next/(?!support|team|resources)"
+            }
+          ]
+        }
+      ]
+    },
+    "image": "img/docusaurus.png",
+    "footer": {
+      "links": [],
+      "copyright": "Copyright © 2021 The Magma Authors",
+      "logo": {
+        "src": "img/magma_icon.png"
+      }
+    },
+    "algolia": {
+      "apiKey": "f95caeb7bc059b294eec88e340e5445b",
+      "indexName": "magma"
+    }
+  }
+}
